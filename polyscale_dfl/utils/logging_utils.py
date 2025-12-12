88659cd4f52892888776e1d7b7b6d01f2bfe4684
Sumit Tapas Chongder <77958774+Sumitchongder@users.noreply.github.com>
2025-12-12 14:06:06 +0530

    Create logging_utils.py

---
+++ polyscale_dfl/utils/logging_utils.py
@@ -0,0 +1,12 @@
+import logging
+import sys
+
+def get_logger(name="PolyScaleFL", level=logging.INFO):
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(level)
+    return logger

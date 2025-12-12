63e418a5464a7e30cfd931625967f363e4b84b45
Sumit Tapas Chongder <77958774+Sumitchongder@users.noreply.github.com>
2025-12-12 13:49:09 +0530

    Create mnist_loader.py

---
+++ polyscale_dfl/datasets/mnist_loader.py
@@ -0,0 +1,11 @@
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
+
+def load_mnist(num_clients=2, batch_size=32):
+    transform = transforms.Compose([transforms.ToTensor()])
+    dataset = datasets.MNIST(root="./data", train=True, download=True, transform=transform)
+    length = len(dataset) // num_clients
+    clients_data = random_split(dataset, [length]*num_clients)
+    loaders = [DataLoader(d, batch_size=batch_size, shuffle=True) for d in clients_data]
+    test_loader = DataLoader(datasets.MNIST(root="./data", train=False, download=True, transform=transform), batch_size=batch_size)
+    return loaders, test_loader
